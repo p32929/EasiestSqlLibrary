d4f1b12b0fbfc2688ef2867b9c69a81babfadef2
p32929 <p32929@yahoo.com>
2020-03-28 16:28:07 +0600

    added license and dummy readme - I will update the readme later

--- app/src/main/java/org/richit/easiestsqlandroidapp/MainActivity.java
+++ app/src/main/java/org/richit/easiestsqlandroidapp/MainActivity.java
@@ -32,44 +32,5 @@ public class MainActivity extends AppCompatActivity {
                         new Column("Column b2", "text", "unique")
                 )
                 .doneAddingTables();
-
-// Add
-        for (int i = 0; i < 5; i++) {
-            boolean done = easiestDB.addDataInTable(0,
-                    new Datum(1, "a" + i),
-                    new Datum(2, "b" + i)
-            );
-
-            Toast.makeText(this, "" + done, Toast.LENGTH_SHORT).show();
-
-            boolean done2 = easiestDB.addDataInTable(1,
-                    new Datum(1, "a" + i),
-                    new Datum(2, "b" + i)
-            );
-
-            Toast.makeText(this, "" + done2, Toast.LENGTH_SHORT).show();
-        }
-
-
-//        Get all data from a table
-//        Cursor cursor = easiestDB.getAllDataFrom(0);
-//        String data = "";
-//        if (cursor != null) {
-//            while (cursor.moveToNext()) {
-//                data += cursor.getString(0) + " " + cursor.getString(1) + " " + cursor.getString(2) + "\n";
-//            }
-//        }
-//
-//        Log.d(TAG, "onCreate: " + data);
-
-
-//        Get one row data from a table
-        Cursor cursor = easiestDB.getOneRowData(0, 2);
-        String data = "";
-        if (cursor != null) {
-            cursor.moveToFirst();
-            data += cursor.getString(0) + " " + cursor.getString(1) + " " + cursor.getString(2) + "\n";
-        }
-        Log.d(TAG, "onCreate: " + data);
     }
 }
